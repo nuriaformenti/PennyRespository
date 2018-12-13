d67d9657774dfddd7b82fa72d91147332402520f
Nuria Formenti <nuria.formenti@nostrasenyora.epiaedu.cat>
2018-12-13 09:12:20 +0100

    Primera Versio

---
+++ Projecte 2D/Assets/CloudMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudMovement : MonoBehaviour {
+
+	public float velocity;
+	void Start () {
+		Debug.Log ("Inici moviment del núvol");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (velocity*Time.deltaTime, 0.0f, 0.0f);
+	}
+}
